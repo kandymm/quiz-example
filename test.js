d4ee48d92eea65c41a1ff21366e4b4540a2d92b9
kandymm <kandymm17@gmail.com>
2023-02-16 18:52:07 +0200

    commit

---
+++ test.js
@@ -0,0 +1,105 @@
+function Soru (soruMetni, cevapSecenekleri, dogruCevap){
+this.soruMetni= soruMetni,
+this.cevapSecenekleri = cevapSecenekleri,
+this.dogruCevap= dogruCevap
+}
+
+Soru.prototype.cevabiKontrolEt= function(cevap){
+    return cevap === this.dogruCevap
+}
+
+let sorular =[
+    new Soru("1-Hangisi javascript paket yönetim uygulamasıdır?",
+    { a: "Node.js", b: "Typescript", c: "Npm" },
+    "c"),
+    new Soru("2-Hangisi .net paket yönetim uygulamasıdır?",
+    { a: "Node.js", b: "Nuget", c: "Npm" },
+    "b"),
+    new Soru(    "3-Hangisi React state yönetim uygulamasıdır?",
+    { a: "Redux", b: "MobX", c: "Flux" },
+    "a"
+),
+    new Soru( "4-Hangisi Vue.js yönetim uygulamasıdır?",
+    { a: "Vuex", b: "React", c: "Angular" },
+    "a")
+]
+
+function Quiz (sorular){
+    this.sorular=sorular,
+    this.soruIndex = 0; 
+}
+
+Quiz.prototype.soruGetir = function(){ 
+    return this.sorular[this.soruIndex]
+}
+
+const quiz = new Quiz(sorular);
+ 
+document.querySelector(".btn_start").addEventListener("click", ()=>{
+        document.querySelector(".quiz_box").classList.add("active")
+        soruGoster(quiz.soruGetir())
+        soruSayisiniGoster(quiz.soruIndex +1, quiz.sorular.length)
+        document.querySelector(".next_btn").classList.remove("show")
+       }
+    )
+
+document.querySelector(".next_btn").addEventListener("click",  function(){
+    if(quiz.sorular.length != quiz.soruIndex +1 ){
+        quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir());
+        document.querySelector(".next_btn").classList.remove("show")
+        soruSayisiniGoster(quiz.soruIndex +1, quiz.sorular.length)
+    }else {
+       console.log("bitti ");
+}
+})
+
+const option_list = document.querySelector(".option_list")
+const correctIcon=`<div class="icon"><i class="fas fa-check"></i></div>`
+const incorrectIcon= `<div class="icon"><i class="fas fa-times"></i></div>`
+
+function soruGoster(soru){
+let question=`<span>${soru.soruMetni}</span>`
+let options = '';
+
+for(let cevap in soru.cevapSecenekleri){
+    options += `
+    <div class = "option">
+    <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
+    </div>`;
+   
+}
+
+document.querySelector(".question_text").innerHTML = question;
+option_list.innerHTML = options;
+
+const option = option_list.querySelectorAll(".option")
+for( let opt of option){
+    opt.setAttribute("onclick", "optionSelected(this)")
+}
+}
+
+function optionSelected(option){
+    let cevap = option.querySelector("span b").textContent
+    let soru = quiz.soruGetir()
+
+    if(soru.cevabiKontrolEt(cevap)){
+        option.classList.add("correct")
+        option.insertAdjacentHTML("beforeend",correctIcon)
+    } else{
+        option.classList.add("incorrect")
+        option.insertAdjacentHTML("beforeend",incorrectIcon)
+    }
+
+
+    for( let i=0; i < option_list.children.length; i++){
+        option_list.children[i].classList.add("disabled")
+    }
+    document.querySelector(".next_btn").classList.add("show")
+}
+
+function soruSayisiniGoster (soruSirasi, toplamSoru){
+
+    let tag =`<span class="badge bg-warning">${soruSirasi} / ${toplamSoru} </span>`
+    document.querySelector(".question_index").innerHTML = tag
+} 
